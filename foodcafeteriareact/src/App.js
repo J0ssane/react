04a4bc7727cbdb06bf75064abe7cf85b268a150a
Jossane Cardoso <88458468+J0ssane@users.noreply.github.com>
2022-03-22 22:24:49 -0300

    Alterado o arquivo App.js

--- foodcafeteriareact/src/App.js
+++ foodcafeteriareact/src/App.js
@@ -1,13 +1,20 @@
 import Food from './components/Food';
+/*Importação do Food*/
 import Footer from './components/Footer';
+/*Importação de Footer*/
 import Header from './components/Header';
+/*Importação de Main*/
 import Main from './components/Main';
-
+/*Função para aplicação*/
 function App() {
+  /*Retorno*/
   return (
     <>
+    /*Fechamento do header*/
       <Header />
+    /*Fechamento do Main*/
       <Main />
+    /*Fechamento do Fooder*/
       <Footer />
     </>
   );
